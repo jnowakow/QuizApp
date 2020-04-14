8b1c19807d66de056a195425e61e2445b697fedb
Jan <janeknowakowski9@gmail.com>
2020-04-14 10:20:54 +0200

    small changes in forms

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=100, help_text="Required. Please enter valid email.")
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-
+    
     #nested name space for configurations
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
